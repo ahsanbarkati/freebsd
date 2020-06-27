1c8d2e677948835035cba784ddc502caa0fc87ce
Ahsan <ahsanbarkati@gmail.com>
2020-06-28 00:25:06 +0200

    Fix minor indent and licence

--- lib/libroute/libroute.c
+++ lib/libroute/libroute.c
@@ -1,5 +1,32 @@
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
+/*
+ * Copyright (c) 2020 Ahsan Barkati
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
 
 #include "libroute.h"
 
@@ -7,7 +34,6 @@ rt_handle *
 libroute_open(int fib)
 {
 	rt_handle *h;
-
 	h = calloc(1, sizeof(*h));
 
 	if (h == NULL)
@@ -79,7 +105,7 @@ libroute_add(rt_handle *h, struct sockaddr* dest, struct sockaddr* gateway){
 }
 
 int
-libroute_change(rt_handle *h, struct sockaddr* dest, struct sockaddr*gateway){
+libroute_change(rt_handle *h, struct sockaddr* dest, struct sockaddr* gateway){
 	int error = libroute_modify(h, dest, gateway, RTM_CHANGE);
 	if(error){
 		err(1, "Failed to change route");
